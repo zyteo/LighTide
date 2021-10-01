23d189f7b0bff2c624a1af95365732556d0d2d92
Stabilo <zteo015@e.ntu.edu.sg>
2021-10-01 17:32:20 +0800

    added wireframe

--- lightide/src/App.js
+++ lightide/src/App.js
@@ -1,11 +1,20 @@
 // GA SEI 32 Project 2: FrontEnd with API 
 // ZY, 1 Oct 2021
-import React from "react";
+import React, { useEffect } from "react";
 import './App.css';
 import Sun from "./Components/Sun";
 import Tides from "./Components/Tides";
 
 function App() {
+
+
+  useEffect(()=>{
+    //geocode API
+fetch("https://api.geoapify.com/v1/geocode/search?text=38%20Upper%20Montagu%20Street%2C%20Westminster%20W1H%201LJ%2C%20United%20Kingdom&apiKey=")
+.then(response => response.json())
+.then(result => console.log(result))
+.catch(error => console.log('error', error));
+  })
   return (
     <div className="App">
       <p>Testing</p>
