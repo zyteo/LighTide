4625d05adb96e798c01683bc0a29ad13f4fd10c0
Stabilo <zteo015@e.ntu.edu.sg>
2021-10-01 12:26:51 +0800

    added tide.jsx

--- lightide/src/App.js
+++ lightide/src/App.js
@@ -3,12 +3,14 @@
 import React from "react";
 import './App.css';
 import Sun from "./Components/Sun";
+import Tides from "./Components/Tides";
 
 function App() {
   return (
     <div className="App">
       <p>Testing</p>
       <Sun/>
+      <Tides/>
     </div>
   );
 }
---
+++ lightide/src/Components/Tides.jsx
@@ -0,0 +1,10 @@
+// GA SEI 32 Project 2: FrontEnd with API
+// ZY, 1 Oct 2021
+
+import React from "react";
+
+function Tides() {
+  return (<div className="tides">tide</div>);
+}
+
+export default Tides;
