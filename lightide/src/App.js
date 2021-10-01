a2b08c0085b131ea465945a5bc89a22a14b770b1
Stabilo <zteo015@e.ntu.edu.sg>
2021-10-01 12:25:18 +0800

    created react app

---
+++ lightide/src/App.js
@@ -0,0 +1,16 @@
+// GA SEI 32 Project 2: FrontEnd with API 
+// ZY, 1 Oct 2021
+import React from "react";
+import './App.css';
+import Sun from "./Components/Sun";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Testing</p>
+      <Sun/>
+    </div>
+  );
+}
+
+export default App;
---
+++ lightide/src/Components/Sun.jsx
@@ -0,0 +1,10 @@
+// GA SEI 32 Project 2: FrontEnd with API
+// ZY, 1 Oct 2021
+
+import React from "react";
+
+function Sun() {
+  return (<div className="sun">test</div>);
+}
+
+export default Sun;
