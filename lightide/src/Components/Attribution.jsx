962a4096a78e0871c0e12116676225b59aafd856
Stabilo <zteo015@e.ntu.edu.sg>
2021-10-02 23:10:03 +0800

    update readme

--- lightide/src/Components/Attribution.jsx
+++ lightide/src/Components/Attribution.jsx
@@ -6,10 +6,15 @@ import React from "react";
 function Attribution() {
   return (
     <footer className="attribution">
-      Sunrise/sunset API from{" "}
-      <a href="https://sunrise-sunset.org/">
-        <span style={{ color: "green" }}> Sunrise Sunset</span>
-      </a>
+      <p>
+        Sunrise/sunset API from
+        <a href="https://sunrise-sunset.org/">
+          <span style={{ color: "green" }}> Sunrise Sunset</span>
+        </a>
+      </p>
+      <p>
+        Nature is unpredictable. Use the information presented at your own risk.
+      </p>
     </footer>
   );
 }
