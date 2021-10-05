cb0908760ef34f5ba58f674b7b62a507ccfb6f6c
Stabilo <zteo015@e.ntu.edu.sg>
2021-10-05 20:40:31 +0800

    changed fetch to axios + added props and details to sun/tide

--- lightide/src/Components/Tides.jsx
+++ lightide/src/Components/Tides.jsx
@@ -4,10 +4,33 @@
 import React from "react";
 
 function Tides({ tide }) {
+  // take the tide prop (JSON data) and map the array, save as tidedetails
+  const tidedetails = tide.data.map((ele) => {
+    return (
+      <>
+        <div className="tidedetails">
+          Date/Time (in UTC): {ele.time}
+          <br />
+          Tide: {ele.type}
+          <br />
+          Height: {ele.height} meters
+        </div>
+      </>
+    );
+  });
+
   return (
     <div className="tides">
       <p>Tides:</p>
-      {tide}
+      <div className="stationdetails">
+        Station name: {tide.meta.station.name}
+        <br />
+        Distance between station and requested coordinate: {tide.meta.station.distance} km
+        
+        <br />
+        Station coordinates: {tide.meta.station.lat} (Latitude), {tide.meta.station.lng} (Longitude)
+      </div>
+      {tidedetails}
     </div>
   );
 }
