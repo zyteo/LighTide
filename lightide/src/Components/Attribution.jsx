0648afbca0c4b03975e31d9ffb615c9b68889b90
Stabilo <zteo015@e.ntu.edu.sg>
2021-10-08 11:40:03 +0800

    styled attribution CSS

--- lightide/src/Components/Attribution.jsx
+++ lightide/src/Components/Attribution.jsx
@@ -2,30 +2,52 @@
 // ZY, 2 Oct 2021
 
 import React from "react";
+import styled from "styled-components";
 
+const A = styled.a`
+  text-decoration: none;
+  color: black;
+`;
+const Span = styled.span`
+  color: navy;
+  border-radius: 10px;
+  &:hover {
+    background-color: gold;
+  }
+  &:active {
+    background-color: yellowgreen;
+  }
+`;
+
+const P = styled.p`
+  margin: 4px;
+`;
 function Attribution() {
   return (
     <footer className="attribution">
-      <p>
-        API Attribution:
-        <a href="https://sunrise-sunset.org/">
-          <span style={{ color: "green" }}> Sunrise Sunset,</span>
-        </a>
-        <a href="https://stormglass.io/marine-weather/">
-          <span style={{ color: "lightblue" }}>Stormglass,</span>
-        </a>
-        <a href="https://www.geoapify.com/">
-          <span style={{ color: "lightblue" }}>Geoapify</span>
-        </a>
-      </p>
-      <p>
-        Nature is unpredictable. Use the information presented at your own risk.
-      </p>
+      <P>
+        Attribution:
+        <A href="https://sunrise-sunset.org/" target="_blank">
+          <Span> Sunrise Sunset,</Span>
+        </A>
+        <A href="https://stormglass.io/marine-weather/" target="_blank">
+          <Span> Stormglass,</Span>
+        </A>
+        <A href="https://www.geoapify.com/" target="_blank">
+          <Span> Geoapify,</Span>
+        </A>
+        <A href="https://www.mapbox.com/" target="_blank">
+          <Span> Mapbox</Span>
+        </A>
+      </P>
+      <P>
+        <strong>Nature is unpredictable.</strong>
+        <em> Use the information presented at your own risk.</em>
+      </P>
       Created Oct 2021 by ZY of GA SEI-32. Github code
-      <a href="https://github.com/zyteo/LighTide">
-        <span> here</span>
-      </a>
-      .
+      <A href="https://github.com/zyteo/LighTide">
+        <Span> here.</Span>
+      </A>
     </footer>
   );
 }
