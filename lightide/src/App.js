70419ec533464e48e6ed50c903fedf7330c93571
Stabilo <zteo015@e.ntu.edu.sg>
2023-08-25 08:06:04 +0800

    set up tide BE api

--- lightide/src/App.js
+++ lightide/src/App.js
@@ -126,8 +126,6 @@ function App() {
   //////////////////////////////// Start of useEffect ////////////////////////////////////////////
   // Geocoding - to get lat/long based on the text searched, renders on every processedText change
   useEffect(() => {
-
-
     // use the BE to get the geocode data
     // axios.get(`https://lightide-be.vercel.app/api/geoapify?text=${processedText}&lang=${searchTextLangugage}`).then((response) => {
     //   // if 400, means no results found. alert user
@@ -141,10 +139,6 @@ function App() {
     //   }
     // }
     // );
-
-
-
-
 
     //geocode API
     axios
@@ -178,6 +172,10 @@ function App() {
       });
 
     //for tide data
+    // use the BE to get the tide data
+    // axios.get(`https://lightide-be.vercel.app/api/tide?lat=${coordinates.lat}&long=${coordinates.long}&date=${date}`).then((response) => {
+    //   setTide(response.data);
+    // });
     axios
       .get(
         `https://api.stormglass.io/v2/tide/extremes/point?lat=${coordinates.lat}&lng=${coordinates.long}&start=${date}&key=${process.env.REACT_APP_TIDE_API_KEY}`
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-require("dotenv").config()
+const axios = require("axios");
+// import "dotenv/config";
+require("dotenv").config();
 const express = require("express");
+// import express from "express";
 const app = express();
 const PORT = process.env.PORT ?? 3000;
-import axios from "axios";
+
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
@@ -16,16 +19,26 @@ app.get("/api/geoapify", (req, res) => {
     )
     .then((response) => {
       if (response.data.features.length === 0) {
-        res.status(400).send("No results found");
+        res.status(400).json({ message: "No results found" });
       } else {
-        res.status(200).send(response.data);
+        // res.status(200).send(response.data);
+        // send as 200, json data
+        res.status(200).json(response.data);
       }
-    }
-    );
-}
+    });
+});
 
-
-
+// set up tide that takes lat, long, and date
+app.get("/api/tide", (req, res) => {
+  const { lat, long, date } = req.query;
+  axios
+    .get(
+      `https://api.stormglass.io/v2/tide/extremes/point?lat=${lat}&lng=${long}&start=${date}&key=${process.env.REACT_APP_TIDE_API_KEY}`
+    )
+    .then((response) => {
+      res.status(200).json(response.data);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.`);
