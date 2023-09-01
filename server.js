4c49443afef625313b894153b728a3858e020fd0
Stabilo <zteo015@e.ntu.edu.sg>
2023-09-01 16:58:33 +0800

    adjusted links

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT ?? 3000;
 
 app.use(
   cors({
-    origin: ["https://lightide.vercel.app", `${process.env.LOCALHOST}`],
+    origin: ["https://lightide.vercel.app"],
   })
 );
 
